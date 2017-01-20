199d04f30f59d9fe2745963f323a5cfb5c6deb24
sujona <jonatan.ostrom@fysik.su.se>
2017-01-20 19:59:54 +0100

    made sourcetest

--- src/localAxes_mod.f90
+++ src/localAxes_mod.f90
@@ -18,12 +18,14 @@ subroutine gs(s,g)
  real(dp), intent(out) :: g(5)
  real(dp) eee, pi, prefac
  integer k
- eee = erf(s)
- pi = 3.1415926535_dp
- prefac = 2.0_dp/dsqrt(pi)*dexp(-s**2) 
+ real(dp), parameter :: two_sqrt_pi = 2.0_dp/dsqrt(3.1415926535_dp)
  
- k=0
- g(1) = eee - prefac * ( 2.0_dp**k * s**(2*k+1) ) / dble(facfac(2*k+1))
+ prefac = two_sqrt_pi*dexp(-s**2) 
+ eee = erf(s)
+ 
+ !k=0
+ !g(1) = eee - prefac * ( 2.0_dp**k * s**(2*k+1) ) / dble(facfac(2*k+1))
+ g(1) = eee - prefac * s 
  
  do k = 1,4
     g(k+1) = g(k) - prefac * ( 2.0_dp**k * s**(2*k+1) ) / dble(facfac(2*k+1))
@@ -33,6 +35,7 @@ end subroutine
 
 
 recursive function facfac(n) result(res)
+! This is the double factorial
  integer, intent(in) :: n
  integer :: res, nn
    res = n
@@ -179,35 +182,4 @@ recursive function norm_2(d) result(d2)
 end function
 
 end module
-
-program test_g
-use localAxes_mod
-real*8 r, gg(5)
-r=0
-do while (r<10.0)
- r=r+0.01
- call gs(r,gg)
- write(*,'(6f10.6)') r,gg
-enddo
-end program
-
-!program test
-!use localAxes_mod
-!use data_types
-!use printer_mod
-!use ps_dms
-!real(dp) a(3),b(3),c(3), d
-!type(h2o) w
-!real(dp) x(3,3)
-!w%h1 = [ 1.80482, -1.73505, -0.85955]!H[,,]
-!w%h2 = [ 2.42260, -0.35826, -0.70808]!H[,,]
-!w%o  = [ 1.66322, -0.92324, -0.35186]!O[,,]
-!b = [0.0_dp, 1.0_dp, 1.0_dp]
-!c = [4.0_dp, 2.0_dp, 2.0_dp]
-!a = cross(b,c)
-!d = dot(b,c)
-!
-!call printer( a,'a')
-!call printer( d,'d')
-!end program
 
