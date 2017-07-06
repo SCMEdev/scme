7630096ddcf5529ddc631456fa8a54ebfbeaca66
Jonatan <jonatan.ostrom@fysik.su.se>
2017-07-06 20:33:30 +0200

    now README.sh is the install script and instructions for putting scme in QUIP including linking altered files

--- quip/README.sh
+++ quip/README.sh
@@ -1,23 +1,27 @@
 # Install SCME in QUIP
+#
+# ln -sf overwrites (-f) the (possibly) existing file with a symbplic (-s) link to the files in this directory. 
 
-#? Alter this line to the QUIP root dir:
+#? Alter this line to the QUIP root dir that you have cloned:
 export QUIP_ROOT=~/gits/QUIP/
 
 #1 Alter sourceMe_env.sh and link to QUIP/
 ln -sf $PWD/sourceMe_quipenv.sh $QUIP_ROOT/sourceMe_quipenv.sh
 
-#2 Create ThirdParty dir. Link/copy things into it. 
-mkdir -f $QUIP_ROOT/src/ThirdParty/
-ln -sf $PWD/cube_tools.f95 $QUIP_ROOT/src/ThirdParty/cube_tools.f95
+#2 Create/keep ThirdParty dir. Link things into it. 
+mkdir -p $QUIP_ROOT/src/ThirdParty/
+ln -sf $PWD/cube_tools.f95 		$QUIP_ROOT/src/ThirdParty/cube_tools.f95
 ln -sf $PWD/Makefile.ThirdParty $QUIP_ROOT/src/ThirdParty/Makefile
-ln -sf $PWD/../../scme $QUIP_ROOT/src/ThirdParty/scme
+rm -f $QUIP_ROOT/src/ThirdParty/scme
+ln -sf $PWD/../../scme 			$QUIP_ROOT/src/ThirdParty/scme
 
 
-#3 Link IPModel_SCME.f95 in QUIP/src/Potenitals/ (substituting the present one)
+#3 Link IPModel_SCME.f95 in QUIP/src/Potenitals/ 
 ln -sf $PWD/IPModel_SCME.f95 $QUIP_ROOT/src/Potentials/IPModel_SCME.f95 
 
-#4 Go to $QUIP_ROOT and source the environment, and install
-(cd $QUIP_ROOT && . sourceMe_quipenv.sh)
-#Only first time, to create Make.inc: (cd $QUIP_ROOT && make config) #only needed once 
-(cd $QUIP_ROOT && make install-quippy)
+##4 Go to $QUIP_ROOT and source the environment, and install
+#cd $QUIP_ROOT 
+#. sourceMe_quipenv.sh)
+##Only first time, to create Make.inc: (cd $QUIP_ROOT && make config) #only needed once 
+#make install-quippy
 
