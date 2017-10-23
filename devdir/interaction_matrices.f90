055195f3ceb27be82c4749b368e12d8583949469
Jonatan <jonatan.ostrom@fysik.su.se>
2017-10-23 09:57:30 +0200

    figuring out how to  formulate the interaction matrices for compressed tensors.

---
+++ devdir/interaction_matrices.f90
@@ -0,0 +1,95 @@
+
+1     2     3     4     5     6     7     8     9     10    11    12    13    14    15    16    17    18    19    20    21
+xxxxx xxxxy xxxxz xxxyy xxxyz xxxzz xxyyy xxyyz xxyzz xxzzz xyyyy xyyyz xyyzz xyzzz xzzzz yyyyy yyyyz yyyzz yyzzz yzzzz zzzzz
+
+g:1  4    4    6    12   6    4    12   12   4    1    4    6    4    1 
+1    2    3    4    5    6    7    8    9    10   11   12   13   14   15
+xxxx xxxy xxxz xxyy xxyz xxzz xyyy xyyz xyzz xzzz yyyy yyyz yyzz yzzz zzzz
+xxxx xxxy xxxz xxyy xxyz xyyy xxzz xyyz xyzz yyyy yyyz xzzz yyzz yzzz zzzz
+
+
+g:1   3   3   3   6   3   1   3   3   1
+n:1   2   3   4   5   6   7   8   9   10 
+  xxx xxy xxz xyy xyz xzz yyy yyz yzz zzz
+
+g:1  2  2  1  2  1
+  xx xy xz yy yz zz 
+
+x y z
+
+!1=4.5
+x: 1 2 3   4 5 6   7  8  9   10 11 12  13 14 15                         
+y: 2 4 5   7 8 9   11 12 13  14 16 17  18 19 20                         
+z: 3 5 6   8 9 10  12 13 14  15 17 18  19 20 21                         
+
+!2=3.5
+xx:1 2 3   4  5  6   7  8  9  10 
+xy:2 4 5   7  8  9   11 12 13 14
+xz:3 5 6   8  9  10  12 13 14 15
+yy:4 7 8   11 12 13  16 17 18 19 
+yz:5 8 9   12 13 14  17 18 19 20
+zz:6 9 10  13 14 15  18 19 20 21
+
+!3=2.5
+xxx: 1 2 3     4  5  6  
+xxy: 2 4 5     7  8  9  
+xxz: 3 5 6     8  9  10 
+xyy: 4 7 8     11 12 13 
+xyz: 5 8 9     12 13 14 
+xzz: 6 9 10    13 14 15 
+yyy: 7 11 12   16 17 18 
+yyz: 8 12 13   17 18 19 
+yzz: 9 13 14   18 19 20  
+zzz: 10 14 15  19 20 21
+
+!2=2.4
+xx:1 2 3   4  5  6  
+xy:2 4 5   7  8  9
+xz:3 5 6   8  9  10
+yy:4 7 8   11 12 13
+yz:5 8 9   12 13 14
+zz:6 9 10  13 14 15
+
+
+g: 1 3 3 3 6 3 1 3 3 1
+1   2   3   4   5   6   7   8   9   10 
+xxx xxy xxz xyy xyz xzz yyy yyz yzz zzz
+
+1     2     3     4     5     6     7     8     9     10    11    12    13    14    15    16    17    18    19    20    21
+xxxxx xxxxy xxxxz xxxyy xxxyz xxxzz xxyyy xxyyz xxyzz xxzzz xyyyy xyyyz xyyzz xyzzz xzzzz yyyyy yyyyz yyyzz yyzzz yzzzz zzzzz
+
+!1=4.5
+1  2  3  4  5  6 
+xx xy xz yy yz zz g: 1 2 2 1 2 1
+
+!1=1.2
+x:1 2 3
+y:2 4 5
+z:3 5 6
+
+
+
+!1=3.4
+x: 1 2 3  4 5 6   7  8  9  10
+y: 2 4 5  7 8 9   11 12 13 14
+z: 3 5 6  8 9 10  12 13 14 15
+
+
+1=1.2:
+x: 1 2 3
+y: 2 4 5
+z: 3 5 6
+
+1=2.3:
+x: 1 2 3 4 5 6
+y: 2 4 5 7 8 9
+z: 3 5 6 8 9 10
+
+2=1.3
+xx:1 2 3
+xy:2 4 5
+xz:3 5 6
+yy:4 7 8
+yz:5 8 9
+zz:6 9 10
+
